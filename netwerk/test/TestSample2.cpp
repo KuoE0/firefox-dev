7a187079818189f16434efe7b5963ad19d6487c5
KuoE0 <kuoe0.tw@gmail.com>
2015-01-14 13:33:43 +0800

    add nsSample2 test

---
+++ netwerk/test/TestSample2.cpp
@@ -0,0 +1,63 @@
+
+#include "TestCommon.h"
+#include "TestHarness.h"
+#include "nsISample2.h"
+#include "prerror.h"
+
+using namespace mozilla;
+
+int
+main()
+{
+  ScopedXPCOM xpcom("Sample2");
+
+  if (xpcom.failed()) {
+    fail("Unable to initialize XPCOM");
+    return -1;
+  }
+
+  nsCOMPtr<nsISample2> sample = do_CreateInstance("@mozilla.org/sample2;1");
+  if (!sample) {
+    fail("Failed to create sample2.");
+    return -1;
+  }
+
+  nsresult rv;
+
+  rv = sample->WriteValue("Inital print:");
+  if (NS_FAILED(rv)) {
+    printf("ERROR: Calling nsISample2::WriteValue() [%x]\n",
+           static_cast<uint32_t>(rv));
+    return -3;
+  }
+
+  const char* testValue = "XPCOM defies gravity";
+  rv = sample->SetValue(testValue);
+  if (NS_FAILED(rv)) {
+    printf("ERROR: Calling nsISample2::SetValue() [%x]\n",
+           static_cast<uint32_t>(rv));
+    return -3;
+  }
+  printf("Set value to: %s\n", testValue);
+  char* str;
+  rv = sample->GetValue(&str);
+
+  if (NS_FAILED(rv)) {
+    printf("ERROR: Calling nsISample2::GetValue() [%x]\n",
+           static_cast<uint32_t>(rv));
+    return -3;
+  }
+  if (strcmp(str, testValue)) {
+    printf("Test FAILED.\n");
+    return -4;
+  }
+
+  NS_Free(str);
+
+  rv = sample->WriteValue("Final print :");
+  printf("Test passed.\n");
+
+  passed("Sample2 Works");
+
+  return 0;
+}
