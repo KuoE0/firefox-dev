9989b7f97c22374108a287e739080f113b5fc739
KuoE0 <kuoe0.tw@gmail.com>
2015-01-15 13:32:17 +0800

    use Mozilla internal string instead of C-style string

--- netwerk/test/TestSample2.cpp
+++ netwerk/test/TestSample2.cpp
@@ -17,48 +17,46 @@ main()
 
   nsresult rv;
 
-  nsCOMPtr<nsISample2> sample = do_CreateInstance("@mozilla.org/sample2;1", &rv);
+  nsCOMPtr<nsISample2> sample =
+    do_CreateInstance("@mozilla.org/sample2;1", &rv);
   if (NS_FAILED(rv)) {
     fail("Failed to create sample2.");
-	printf("Error code: 0x%X\n", rv);
+    printf("Error code: 0x%X\n", rv);
     return -1;
   }
 
-
-  rv = sample->WriteValue("Inital print:");
+  rv = sample->WriteValue(NS_LITERAL_CSTRING("Inital print:"));
   if (NS_FAILED(rv)) {
     printf("ERROR: Calling nsISample2::WriteValue() [%x]\n",
            static_cast<uint32_t>(rv));
-	printf("Error code: 0x%X\n", rv);
+    printf("Error code: 0x%X\n", rv);
     return -3;
   }
 
-  const char* testValue = "XPCOM defies gravity";
+  nsCString testValue = NS_LITERAL_CSTRING("XPCOM defies gravity");
   rv = sample->SetValue(testValue);
   if (NS_FAILED(rv)) {
     printf("ERROR: Calling nsISample2::SetValue() [%x]\n",
            static_cast<uint32_t>(rv));
-	printf("Error code: 0x%X\n", rv);
+    printf("Error code: 0x%X\n", rv);
     return -3;
   }
-  printf("Set value to: %s\n", testValue);
-  char* str;
-  rv = sample->GetValue(&str);
+  printf("Set value to: %s\n", ToNewCString(testValue));
+  nsCString str;
+  rv = sample->GetValue(str);
 
   if (NS_FAILED(rv)) {
     printf("ERROR: Calling nsISample2::GetValue() [%x]\n",
            static_cast<uint32_t>(rv));
-	printf("Error code: 0x%X\n", rv);
+    printf("Error code: 0x%X\n", rv);
     return -3;
   }
-  if (strcmp(str, testValue)) {
+  if (!str.Equals(testValue)) {
     printf("Test FAILED.\n");
     return -4;
   }
 
-  NS_Free(str);
-
-  rv = sample->WriteValue("Final print :");
+  rv = sample->WriteValue(NS_LITERAL_CSTRING("Final print :"));
   printf("Test passed.\n");
 
   passed("Sample2 Works");
