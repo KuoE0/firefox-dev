a27a685831a972ab94a2e13e7ed6eac3f462d6b3
KuoE0 <kuoe0.tw@gmail.com>
2015-01-19 10:17:50 +0800

    add WebLock component

---
+++ mine/src/nsWebLock.cpp
@@ -0,0 +1,76 @@
+/*
+ * nsWebLock.cpp
+ * Copyright (C) 2015 KuoE0 <kuoe0.tw@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+#include "nsWebLock.h"
+#include "nsCOMPtr.h"
+#include "nsIMutableArray.h"
+#include "nsComponentManagerUtils.h"
+#include "nsISupportsPrimitives.h"
+
+NS_IMPL_ISUPPORTS(nsWebLock, nsIWebLock)
+
+nsWebLock::nsWebLock()
+{
+}
+
+nsWebLock::~nsWebLock()
+{
+}
+
+	NS_IMETHODIMP
+nsWebLock::Lock()
+{
+	mLocked = PR_TRUE;
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsWebLock::Unlock()
+{
+	mLocked = PR_FALSE;
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsWebLock::AddSite(const nsAString & url)
+{
+	for (auto x: mSites)
+		if (url.Equals(x)) return NS_ERROR_FAILURE;
+	mSites.push_back(nsString(url));
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsWebLock::RemoveSite(const nsAString & url)
+{
+	vector<nsString>::iterator iter;
+	for (iter = mSites.begin(); iter != mSites.end(); ++iter) {
+		if (url.Equals(*iter)) break;
+	}
+	
+	if (iter == mSites.end()) return NS_ERROR_FAILURE;
+
+	mSites.erase(iter);
+
+	return NS_OK;
+}
+
+NS_IMETHODIMP
+nsWebLock::GetSites(nsIMutableArray * *aSites)
+{
+	nsCOMPtr<nsIMutableArray> array = do_CreateInstance(NS_ARRAY_CONTRACTID);
+	for (auto url: mSites) {
+		nsCOMPtr<nsISupportsString> iUrl = do_CreateInstance(NS_SUPPORTS_STRING_CONTRACTID);
+		iUrl->SetData(url);
+		array->AppendElement(iUrl, PR_FALSE);
+	}
+
+	*aSites = array;
+	NS_ADDREF(*aSites);
+	return NS_OK;
+}
+
---
+++ mine/src/nsWebLock.h
@@ -0,0 +1,42 @@
+/*
+ * nsWebLock.h
+ * Copyright (C) 2015 KuoE0 <kuoe0.tw@gmail.com>
+ *
+ * Distributed under terms of the MIT license.
+ */
+
+
+#ifndef nsWebLock_h__
+#define nsWebLock_h__
+
+#include "nsString.h"
+#include "nsIWebLock.h"
+#include <vector>
+
+using namespace std;
+
+#define NS_WEBLOCK_CID                                                         \
+  {                                                                            \
+	0xea54eee4, 0x9548, 0x4b63,                                                \
+    {                                                                          \
+      0xb9, 0x4d, 0xc5, 0x19, 0xff, 0xc9, 0x1d, 0x9                            \
+    }                                                                          \
+  }
+
+#define NS_WEBLOCK_CONTRACTID "@mozilla.org/weblock;1"
+
+class nsWebLock MOZ_FINAL: public nsIWebLock
+{
+public:
+	nsWebLock();
+	NS_DECL_ISUPPORTS
+	NS_DECL_NSIWEBLOCK
+private:
+	~nsWebLock();
+	PRBool mLocked;
+	vector<nsString> mSites;
+};
+
+
+#endif /* !nsWebLock_h__ */
+
