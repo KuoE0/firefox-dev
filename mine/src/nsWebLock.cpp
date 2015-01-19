b1aa6b5867c0b389c7b09956a24c1748d3784be1
KuoE0 <kuoe0.tw@gmail.com>
2015-01-19 10:20:13 +0800

    apply mozilla coding style

--- mine/src/nsWebLock.cpp
+++ mine/src/nsWebLock.cpp
@@ -13,64 +13,63 @@
 
 NS_IMPL_ISUPPORTS(nsWebLock, nsIWebLock)
 
-nsWebLock::nsWebLock()
-{
-}
+nsWebLock::nsWebLock() {}
 
-nsWebLock::~nsWebLock()
-{
-}
+nsWebLock::~nsWebLock() {}
 
-	NS_IMETHODIMP
+NS_IMETHODIMP
 nsWebLock::Lock()
 {
-	mLocked = PR_TRUE;
-	return NS_OK;
+  mLocked = PR_TRUE;
+  return NS_OK;
 }
 
 NS_IMETHODIMP
 nsWebLock::Unlock()
 {
-	mLocked = PR_FALSE;
-	return NS_OK;
+  mLocked = PR_FALSE;
+  return NS_OK;
 }
 
 NS_IMETHODIMP
-nsWebLock::AddSite(const nsAString & url)
+nsWebLock::AddSite(const nsAString &url)
 {
-	for (auto x: mSites)
-		if (url.Equals(x)) return NS_ERROR_FAILURE;
-	mSites.push_back(nsString(url));
-	return NS_OK;
+  for (auto x : mSites)
+    if (url.Equals(x))
+      return NS_ERROR_FAILURE;
+  mSites.push_back(nsString(url));
+  return NS_OK;
 }
 
 NS_IMETHODIMP
-nsWebLock::RemoveSite(const nsAString & url)
+nsWebLock::RemoveSite(const nsAString &url)
 {
-	vector<nsString>::iterator iter;
-	for (iter = mSites.begin(); iter != mSites.end(); ++iter) {
-		if (url.Equals(*iter)) break;
-	}
-	
-	if (iter == mSites.end()) return NS_ERROR_FAILURE;
+  vector<nsString>::iterator iter;
+  for (iter = mSites.begin(); iter != mSites.end(); ++iter) {
+    if (url.Equals(*iter))
+      break;
+  }
 
-	mSites.erase(iter);
+  if (iter == mSites.end())
+    return NS_ERROR_FAILURE;
 
-	return NS_OK;
+  mSites.erase(iter);
+
+  return NS_OK;
 }
 
 NS_IMETHODIMP
-nsWebLock::GetSites(nsIMutableArray * *aSites)
+nsWebLock::GetSites(nsIMutableArray **aSites)
 {
-	nsCOMPtr<nsIMutableArray> array = do_CreateInstance(NS_ARRAY_CONTRACTID);
-	for (auto url: mSites) {
-		nsCOMPtr<nsISupportsString> iUrl = do_CreateInstance(NS_SUPPORTS_STRING_CONTRACTID);
-		iUrl->SetData(url);
-		array->AppendElement(iUrl, PR_FALSE);
-	}
+  nsCOMPtr<nsIMutableArray> array = do_CreateInstance(NS_ARRAY_CONTRACTID);
+  for (auto url : mSites) {
+    nsCOMPtr<nsISupportsString> iUrl =
+      do_CreateInstance(NS_SUPPORTS_STRING_CONTRACTID);
+    iUrl->SetData(url);
+    array->AppendElement(iUrl, PR_FALSE);
+  }
 
-	*aSites = array;
-	NS_ADDREF(*aSites);
-	return NS_OK;
+  *aSites = array;
+  NS_ADDREF(*aSites);
+  return NS_OK;
 }
-
--- mine/src/nsWebLock.h
+++ mine/src/nsWebLock.h
@@ -5,7 +5,6 @@
  * Distributed under terms of the MIT license.
  */
 
-
 #ifndef nsWebLock_h__
 #define nsWebLock_h__
 
@@ -17,7 +16,7 @@ using namespace std;
 
 #define NS_WEBLOCK_CID                                                         \
   {                                                                            \
-	0xea54eee4, 0x9548, 0x4b63,                                                \
+    0xea54eee4, 0x9548, 0x4b63,                                                \
     {                                                                          \
       0xb9, 0x4d, 0xc5, 0x19, 0xff, 0xc9, 0x1d, 0x9                            \
     }                                                                          \
@@ -25,18 +24,16 @@ using namespace std;
 
 #define NS_WEBLOCK_CONTRACTID "@mozilla.org/weblock;1"
 
-class nsWebLock MOZ_FINAL: public nsIWebLock
+class nsWebLock MOZ_FINAL : public nsIWebLock
 {
 public:
-	nsWebLock();
-	NS_DECL_ISUPPORTS
-	NS_DECL_NSIWEBLOCK
+  nsWebLock();
+  NS_DECL_ISUPPORTS
+  NS_DECL_NSIWEBLOCK
 private:
-	~nsWebLock();
-	PRBool mLocked;
-	vector<nsString> mSites;
+  ~nsWebLock();
+  PRBool mLocked;
+  vector<nsString> mSites;
 };
 
-
 #endif /* !nsWebLock_h__ */
-
